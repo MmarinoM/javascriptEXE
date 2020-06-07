a57613817f653e950bf8f2e8f749048f5a59a534
Michael <marino.michael.1990@gmail.com>
2020-06-07 15:48:15 +0200

    exe 03.02

--- 03-colors/02-change-bcg-two/script.js
+++ 03-colors/02-change-bcg-two/script.js
@@ -11,6 +11,8 @@
 
 (function() {
 
-    // your code here
-
+    document.getElementById("run").addEventListener("click", function(){
+        let color = document.getElementById("color").value;
+        document.body.style.backgroundColor = color;
+    });
 })();
