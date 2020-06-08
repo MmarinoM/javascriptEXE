c2f5eab2eeb77f26bd095c85035f452e182f2401
MmarinoM <marino.michael.1990@gmail.com>
2020-06-08 11:41:47 +0200

    exe 05.09

--- 05-arrays/09-rand-array-stats/script.js
+++ 05-arrays/09-rand-array-stats/script.js
@@ -11,6 +11,32 @@
 
 (function() {
 
-    // your code here
+    document.getElementById("run").addEventListener("click", function(){
+        let tabNum = new Array();
+        let add = 0;
+        
+        for (let index = 0; index < 10; index++) {
+            
+            tabNum[index] = Math.floor(Math.random() * 101);
+            add = add + tabNum[index];
+            document.getElementById("n-"+(index+1)).innerHTML = tabNum[index];
+        }
+        
+        let min = Math.min.apply( Math, tabNum );
+        let max = Math.max.apply( Math, tabNum );
+        let average = add / tabNum.length;
+        
+        console.log(average);
+        console.log(add);
+        console.log(tabNum);
+        console.log(min);
+        console.log(max);
+
+        document.getElementById("min").innerHTML = min;
+        document.getElementById("max").innerHTML = max;
+        document.getElementById("sum").innerHTML = add;
+        document.getElementById("average").innerHTML = average;
+
+    });
 
 })();
