4d040bf5427aed95e083cb7edbe71f132e3ba341
MmarinoM <marino.michael.1990@gmail.com>
2020-06-11 08:42:16 +0200

    THE END

--- 08-dom/11-change-event-input-one/script.js
+++ 08-dom/11-change-event-input-one/script.js
@@ -10,7 +10,22 @@
 // You will have time to focus on it later.
 
 (function() {
+    let count = 0;
+    document.getElementById("pass-one").addEventListener("input",function(nbwords){
+        console.log(nbwords);
 
-    // your code here
+        if(nbwords.inputType == "insertText"){
+            count = count + 1;
+        }else if(nbwords.inputType == "deleteContentBackward"){
+            count = count - 1;
+        }
+
+        console.log(count);
+        // // let nbWords = InputEvent.length;
+        document.getElementById("counter").innerHTML = count+"/10";
+    });
+    document.querySelector("input").maxLength = 10;
+
+    
 
 })();
